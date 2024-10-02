19aa285ad1dc0c250fc8bd888fa315958562d5ae
Kai Yu <kaifany1@uci.edu>
2024-10-02 03:01:08 -0700

    remove

--- src/lib/Skills.ts
+++
@@ -1,8 +0,0 @@
-const skills = {
-	Programming: ['C++', 'C#', 'Python', 'HTML/CSS', 'Javascript', 'Svelte'],
-	Cybersecurity: ['Ethical Hacking', 'Vulnerability Management ', 'Threat Modeling','Identity and Access Management', 'Cloud Security','Subnetting'], 
-	tools: ['Kali Linux', 'VS Code', 'Github', 'Raspberry Pi', 'Nessus'],
-	Languages: ['English', 'Mandarin', 'Japanese'],
-};
-
-export default skills;
