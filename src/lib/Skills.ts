57433484c1455121ff7c90e343d6390760b8809b
Kai Yu <kaifany1@uci.edu>
2024-10-03 00:04:26 -0700

    upload

--- src/lib/Skills.ts
+++
@@ -1,8 +0,0 @@
-const skills = {
-	Programming: ['C++', 'C#', 'Python', 'HTML/CSS', 'Javascript', 'Svelte', 'Kotlin', 'Powershell'],
-	Cybersecurity: ['Ethical Hacking', 'Vulnerability Management ', 'Threat Modeling','Identity and Access Management', 'Cloud Security','Subnetting'], 
-	Tools: ['Kali Linux', 'VS Code', 'Github', 'Raspberry Pi', 'Nessus'],
-	Languages: ['English', 'Mandarin', 'Japanese'],
-};
-
-export default skills;
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,120 +1,26 @@
-<script>
-	import skills from '$lib/Skills';
-	
-</script>
-
 <svelte:head>
-	<title>Kai | About</title>
+	<title>About</title>
+	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="container">
-	<main>
-		<br>
-		<br>
+<div class="text-column">
+	<h1>About this app</h1>
 
-		<span class="about-header">About</span>
-		<br>
-		<br>
-		<p class="about-description">
-			I am a passionate computer science student with a deep interest in software engineering and information security. Through my academic journey and personal projects, I have honed my skills in coding, problem-solving, and ensuring the integrity of digital systems. My goal is to leverage my knowledge and enthusiasm to contribute to the ever-evolving world of technology. 
-		</p>
-	</main>
-	<br>
-	<br>
-	<br>
-	<span class="skill-about">Skills</span>
-	<div class="skill-div">
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+		following into your command line and following the prompts:
+	</p>
 
-		{#each Object.entries(skills) as [section, technologies]}
-		<br>
-				<p class="skillset">
-					{section}:
-				<p>
-				<div class="description">
-					{#each technologies as technology}
-						<p>
-							{technology}
-						</p>
-					{/each}
-				</div>
-		{/each}
-		</div>
+	<pre>npm create svelte@latest</pre>
 
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
 
-		
-
+	<p>
+		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
+		using it with JavaScript disabled!
+	</p>
 </div>
-
-<style>
-
-	
-	* {
-		margin: 0;
-		padding: 0;
-		height: auto;
-		line-height: 2rem;
-}
-
-	.container {
-		padding: 0;
-		margin: 0;
-		max-width: 1200px;
-		justify-content: left;
-	}
-
-	main {
-
-		padding: 0;
-		margin: 0;
-		max-width: 100%;
-		text-align: left;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-	.about-header{
-		margin: 20px 20px;
-		font-size: 3rem;
-		color: #4158d0;
-	}
-	.about-description{
-		margin: 20px 20px;
-		font-size: 1.3rem
-	}
-	.skill-about{
-		font-size: 1.7rem;
-		color: #4158d0;
-		margin: 20px 20px;
-	}
-	.skill-div {
-		margin: 20px 20px;
-
-  /* Add any desired styles for the skill-div container here */
-	}
-
-	.skillset {
-		font-weight: bold; /* Make section text bold */
-		font-size: 1.3rem;
-		margin-bottom: 5px; /* Add space between sections */
-	}
-
-	.description {
-		display: flex; /* Use flexbox to arrange technologies horizontally */
-		flex-wrap: wrap; /* Allow technologies to wrap to the next line if necessary */
-		gap: 30px; /* Add a gap between technology items */
-	}
-
-
-
-	@media screen and (max-width: 900px) {
-
-		.about-description, .skill-about , 
-		.about-header{
-			margin: 20px 3rem;
-		}
-		.skillset, .description
-		{
-			margin: 20px 2rem;
-		}
-	}
-
-</style>
